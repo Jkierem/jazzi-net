ee62687187caf5374ada3d884ee93af7d8e1c3b5
Jkierem <juanmiguelgomez8@gmail.com>
2023-06-13 10:11:23 -0500

    Release prepped

--- scripts/release.sh
+++
@@ -1,9 +0,0 @@
-git branch -D release
-git push origin --delete release
-git checkout -b release
-rm -rf ./examples
-rm -rf ./scripts
-rm velociraptor.json
-git add -A 
-git commit -m "Release prepped"
-git push --set-upstream origin release
